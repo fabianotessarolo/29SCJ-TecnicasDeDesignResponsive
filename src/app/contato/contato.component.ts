dbe7fb5ce3f006fd09d8b09829b9ffbeeddd96d8
Fabiano Tessarolo <fabiano.tessarolo@gmail.com>
2017-06-18 22:50:06 -0300

    changed contato.components.ts

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -7,17 +7,31 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
   styleUrls: ['./contato.component.css']
 })
 export class ContatoComponent implements OnInit {
+  falha = {sucesso: false, texto: "Problemas no envio do formulário de contato"};
+  contatoForm = FormGroup;
+  listaErros = [];
+  listaResultado = [];
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  this.buildForm();
-}
+  mensagensErro = {
+     'nome': { 'required': 'favor preencher o nome'},
+     'email': { 
+        'required': 'favor preencher o email',
+        'emailIsValid': 'o formato do email esta inválido' },
+     'texto': {
+       'required': 'favor preencher o texto',
+       'minLength': 'Voce precisa informar no minimo 5 caracteres',
+       'maxLength': 'O limite é de 100 caracteres'
+      }
+  }   
+
+  ngOnInit() {this.buildForm(); }
 
 buildForm() {
   this.contatoForm = this.fb.group({
     'nome': ['', [Validators.required]],
-    'email': ['', [Validators.required]],
+    'email': ['', [Validators.required, EmailValidator.emailIsValid]],
     'texto': ['', [Validators.required,
     Validators.minLength(4), Validators.maxLength(100)]]
   })
